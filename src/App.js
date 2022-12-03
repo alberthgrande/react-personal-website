fef2a7e14bdd25a71a449ad25fd904d3da37c42f
Alberth G. Ruado <alberthruado@gmail.com>
2022-12-03 18:11:46 +0800

    Update Blog Section

--- src/App.js
+++ src/App.js
@@ -1,30 +1,72 @@
+import Navbar from "./components/Navbar";
 import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
 
 function App() {
   return (
-    <Form>
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <div>
+      <Navbar />
+      <div className="container">
+        {/* home */}
+        <section id="Home">
+          <article>
+            <h1>Home </h1>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </article>
+        </section>
+        {/* about */}
+        <section id="About">
+          <article>
+            <h1>About </h1>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </article>
+        </section>
+        {/* constact */}
+        <section id="Contact">
+          <article>
+            <h1>Contact </h1>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry...
+          </article>
+        </section>
+        {/* blog */}
+        <section id="Blog">
+          <article>
+            <h1>Blog </h1>
+            <p>How to deploy Static React Websit in Github</p> <br />
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+            <div className="create-react">
+              <h4>[1] Create a react app first using:</h4>
+              <p>npx create-react-app my-react</p> <br />
+            </div>
+
+            <div className="intall-libraries">
+              <h4>[2] Install this npm libraries:</h4>
+              <p>npm install gh-pages --save-dev</p><br />
+            </div>
+
+            <div className="git-command">
+              <h4>[3] Apply this git commad:</h4>
+              <ul>
+                <li>git init</li>
+                <li>git add .</li>
+                <li>git status</li>
+                <li>git commit -m "first commit"</li>
+                <li>git branch -M main</li>
+                <li>
+                  git remote add origin https://github.com/github-username/github-repository-name.git
+                </li>
+                <li>git push -u origin main</li>
+              </ul>
+            </div>
+            <br />
+
+            <div className="intall-libraries">
+              <h4>[4] And then run this npm libraries: </h4>
+              <p>npm run deploy</p>
+            </div>
+          </article>
+        </section>
+      </div>
+    </div>
   );
 }
 
